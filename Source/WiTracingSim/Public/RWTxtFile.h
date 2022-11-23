6ed70d1bc762f2a5fa97db0dca1a22a318ebe50f
yzha0058 <yu.zhang1@sydney.edu.au>
2022-11-24 00:07:48 +1100

    add read write to file

---
+++ Source/WiTracingSim/Public/RWTxtFile.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Kismet/BlueprintFunctionLibrary.h"
+#include "RWTxtFile.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class WITRACINGSIM_API URWTxtFile : public UBlueprintFunctionLibrary
+{
+	GENERATED_BODY()
+	
+};
