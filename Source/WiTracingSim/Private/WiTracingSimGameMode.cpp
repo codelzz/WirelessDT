14fa0801200202c71c84577b756243d680ff1ca1
code <31692702+codelzz@users.noreply.github.com>
2023-01-06 10:32:36 +0800

    change asset folder

--- Source/WiTracingSim/Private/WiTracingSimGameMode.cpp
+++ Source/WiTracingSim/Private/WiTracingSimGameMode.cpp
@@ -7,7 +7,7 @@
 AWiTracingSimGameMode::AWiTracingSimGameMode()
 {
 	// set default pawn class to our Blueprinted character
-	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnBPClass(TEXT("/Game/ThirdPerson/Blueprints/BP_ThirdPersonCharacter"));
+	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnBPClass(TEXT("/Game/Characters/BP_ThirdPersonCharacter"));
 	if (PlayerPawnBPClass.Class != NULL)
 	{
 		DefaultPawnClass = PlayerPawnBPClass.Class;
