7fe4549d978ae9af8fbb27982daf29e0fbb564b0
ZYWZ <yzha0058@gmail.com>
2023-04-04 23:10:31 +1000

    implemented camera detection

--- Source/WiTracingPy/RL/WIreless_encoder/TXDataset.py
+++ Source/WiTracingPy/RL/WIreless_encoder/TXDataset.py
@@ -22,22 +22,32 @@ class TXDataset(Dataset):
     def __len__(self):
         return len(self.data)
 
+    def pad_list(self, rssi_list, target_length=20, padding_value=-255):
+        padded_list = rssi_list.copy()
+        while len(padded_list) < target_length:
+            padded_list.append(padding_value)
+        padded_list = padded_list[:20]
+        return padded_list
+
     def __getitem__(self, idx):
         # Get the TXName string from the data
         tx_names = self.data.iloc[idx]['tx']
+        tx_rssis = self.data.iloc[idx]['rssi']
 
         # Pad each string with spaces to make them have equal length
-        padded_tx_names = [s.ljust(self.pad_length, self.pad_value) for s in tx_names]
+        # padded_tx_names = [s.ljust(self.pad_length, self.pad_value) for s in tx_names]
+        padded_tx_rssis = self.pad_list(tx_rssis)
 
-        while len(padded_tx_names) < 20:
-            padded_tx_names.append(self.pad_string)
-        if len(padded_tx_names) >= 20:
-            padded_tx_names = padded_tx_names[:20]
-        padded_tx_names = ' '.join(padded_tx_names)
-        label = (self.data.iloc[idx]['x'], self.data.iloc[idx]['x'], self.data.iloc[idx]['z'])
+        while len(tx_names) < 20:
+            tx_names.append(self.pad_value)
+        if len(tx_names) >= 20:
+            tx_names = tx_names[:20]
+        tx_names = ' '.join(tx_names)
+        label = (self.data.iloc[idx]['x'], self.data.iloc[idx]['y'], self.data.iloc[idx]['z'])
 
         # Convert the TXName string to a sequence of token ids
-        tx_name_ids = torch.tensor(self.tokenizer.encode(padded_tx_names, padding='max_length', max_length=1024, add_special_tokens=True))
+        tx_name_ids = torch.tensor(self.tokenizer.encode(tx_names, padding='max_length', max_length=1024, add_special_tokens=True))
         # print(tx_name_ids.shape, padded_tx_names, len(padded_tx_names))
+        # rssi_tensor = torch.tensor(padded_tx_rssis, dtype=torch.int64)
 
-        return tx_name_ids, label
+        return tx_name_ids, padded_tx_rssis, label
--- Source/WiTracingPy/RL/WIreless_encoder/model.py
+++ Source/WiTracingPy/RL/WIreless_encoder/model.py
@@ -1,3 +1,4 @@
+import torch
 from torch import nn
 
 
@@ -6,24 +7,35 @@ class WirelessEncoder(nn.Module):
         super(WirelessEncoder, self).__init__()
         # Define the embedding layer
         self.embedding_layer = nn.Embedding(num_words, embedding_dim)
+        self.rssi_embedding_layer = nn.Embedding(256, embedding_dim)
 
         # Define the LSTM layer
         self.lstm_layer = nn.LSTM(embedding_dim, hidden_size, batch_first=True)
+        self.rssi_layer = nn.Linear(20*embedding_dim, 512)
 
         # Define the dense output layer
         self.output_layer = nn.Linear(hidden_size, 2)
 
-        self.result_layer = nn.Linear(max_length*2, 3)
+        self.result_layer1 = nn.Linear(max_length*2, 512)
+        self.result_layer2 = nn.Linear(1024, 128)
+        self.result_layer3 = nn.Linear(128, 3)
 
     def forward(self, input_data):
+        txname, rssis = input_data
         # Perform feedforward calculations
-        embedding_output = self.embedding_layer(input_data)
+        embedding_output = self.embedding_layer(txname)
+        rssi_embedding_output = self.rssi_embedding_layer(rssis)
+
         lstm_output, _ = self.lstm_layer(embedding_output)
+        rssi_output = self.rssi_layer(rssi_embedding_output.flatten(start_dim=1))
 
         output = self.output_layer(lstm_output)
-        # print(output.shape)
 
-        result = self.result_layer(output.flatten(start_dim=1))
+        x = self.result_layer1(output.flatten(start_dim=1))
+        x = torch.cat((x, rssi_output), 1)
+
+        x = self.result_layer2(x)
+        result = self.result_layer3(x)
 
         # Return output
         return result
